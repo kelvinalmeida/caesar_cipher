1781e0c3c6e8e54739fb0f5e9281399a51c31f35
Kelvin Santos <kelvinsantosdealmeida@gmail.com>
2024-10-31 21:06:53 -0300

    finish

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,12 @@
 alfabet = ("a".."z").to_a
 
+def is_upcase?(char)
+  char == char.upcase
+end
+
 def find_positions_down(char, number_of_positions_down, alfabet)
+
+  char.downcase!
 
   return if !alfabet.index(char) # returns if there is no char in the alphabet
 
@@ -13,17 +19,21 @@ def find_positions_down(char, number_of_positions_down, alfabet)
   else
     char_new_index_down
   end
+
+  return char_new_index_down
 end
 
 def caesar_cipher(string, number_of_positions_down, alfabet)
-  array_of_chars = string.downcase.chars
+  # array_of_chars = string.chars
   new_string = Array.new
 
-  array_of_chars.each do |char|
+  string.each_char do |char|
     new_char_index = find_positions_down(char, number_of_positions_down, alfabet)
 
     if new_char_index # if new_char_index is no nil
-      new_string.push(alfabet[new_char_index])
+      # new_char = is_upcase?(char) ? alfabet[new_char_index].upcase : alfabet[new_char_index]
+      new_char = alfabet[new_char_index]
+      new_string.push(new_char)
     else
       new_string.push(char)
     end
